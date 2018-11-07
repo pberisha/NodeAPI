116a8e784ac8970e3bab1702fce86dfa9b5512bb
Petrit Berisha <petrit.berisha@led-con.com>
2018-11-07 01:25:17 +0100

    Fixed MongoDB Connection

--- src/index.js
+++ src/index.js
@@ -4,35 +4,33 @@ const PORT = 8080;
 const HOST = "0.0.0.0";
 
 const app = express();
+mongoose.connect("mongodb://root:example@mongo:27017/express?authSource=admin");
+const Customer = mongoose.model("Customer", { 
+    name: String,
+    surname: String,
+    address: String,
+    telefon: String
+  });
 
-app.get("/", (req, res) => {
-  const options = {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    autoIndex: false, // Don't build indexes
-    reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-    reconnectInterval: 500, // Reconnect every 500ms
-    poolSize: 10, // Maintain up to 10 socket connections
-    // If not connected, return errors immediately rather than waiting for reconnect
-    bufferMaxEntries: 0,
-    connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
-    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    family: 4 // Use IPv4, skip trying IPv6
-  };
+  app.get("/genNewCustomer", (req, res) => {
+    const Cust = new Customer({ 
+      name: "Arber", 
+      surname: "Marleku", 
+      address: "Rr. Jakove Xoxa C44, Prishtine",
+      telefon: "049600432"
+    });
+    Cust.save().then(() => res.json(Cust));
+  });
 
-  mongoose.connect(
-    "mongodb://root:example@mongo:27017/local",
-    options
-  );
+  app.get("/getCustomer", (req, res) => {
+    const Cust = Customer.find({ name: 'Arber' }, function (err, Customer) {
+      res.json(Customer);
+    });
+  });
 
-  const Cat = mongoose.model("Cat", { name: String });
-
-  const kitty = new Cat({ name: "Zildjian" });
-  kitty.save().then(() => console.log("meow"));
-
-  res.send("Hello world, How its Going?\n");
-});
+  app.get("/", (req, res) => {
+    res.send("Hello world, How its Going?\n");
+  });
 
 app.listen(PORT);
 console.log(`Running on http://${HOST}:${PORT}`);
